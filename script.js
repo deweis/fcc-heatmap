a56d3d9943ebc2b164cb0e18a4e36e41304c85c9
Dennis <deweis@gmx.ch>
2018-12-25 11:15:15 +0100

    Convert the data

--- script.js
+++ script.js
@@ -6,5 +6,33 @@ fetch(
     return response.json();
   })
   .then(function(data) {
-    console.log(data);
+    updateChart(data);
   });
+
+/********** Create the chart *********/
+function updateChart(data) {
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+
+  const dataset = data.monthlyVariance.map(x => ({
+    year: x.year,
+    month: x.month,
+    monthName: months[x.month - 1],
+    variance: x.variance.toFixed(1),
+    temperature: (8.66 + x.variance).toFixed(1)
+  }));
+
+  console.log(dataset);
+}
