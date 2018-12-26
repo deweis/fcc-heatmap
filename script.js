0936b887c10f94f3f5756a4a9d23dfa39ca5786d
Dennis <deweis@gmx.ch>
2018-12-26 10:10:14 +0100

    Add the svg and xscale

--- script.js
+++ script.js
@@ -35,4 +35,34 @@ function updateChart(data) {
   }));
 
   console.log(dataset);
+
+  const w = 900;
+  const h = 450;
+
+  /* Padding between the SVG canvas boundary and the plot */
+  const padding = 55;
+
+  /* Create an x and y scale */
+  const minX = d3.min(dataset, d => d[0]);
+  const maxX = d3.max(dataset, d => d[0]);
+
+  const xScale = d3
+    .scaleLinear()
+    .domain([minX, maxX])
+    .range([0, w - 1.5 * padding]);
+
+  /* Add the SVG */
+  const svg = d3
+    .select('#chartContainer')
+    .append('svg')
+    .attr('id', 'chart')
+    .attr('width', w)
+    .attr('height', h)
+    .style('fill', '#757575') // grey darken-1
+    /*
+    Make it responsive
+      Thank you: https://stackoverflow.com/a/9539361 resp. http://jsfiddle.net/shawnbot/BJLe6/
+  */
+    .attr('viewBox', `0 0 ${w} ${h}`)
+    .attr('preserveAspectRatio', 'xMidYMid');
 }
